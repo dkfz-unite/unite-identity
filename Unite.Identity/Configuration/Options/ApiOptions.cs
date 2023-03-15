9999a97318cc2070014df0e538fc4282d7b26d18
DGitRich <daniel.rich@gmx.net>
2023-03-15 15:55:17 +0100

    Create SignInController with supplements

---
+++ Unite.Identity/Configuration/Options/ApiOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Unite.Identity.Configuration.Options;
+
+public class ApiOptions
+{
+    public byte[] Key
+    {
+        get
+        {
+            var key = Environment.GetEnvironmentVariable("UNITE_API_KEY");
+
+            if (key == null)
+                throw new ArgumentNullException("'UNITE_API_KEY' environment variable has to be set");
+
+            if (key.Length != 32)
+                throw new ArgumentOutOfRangeException("'UNITE_API_KEY' environment variable has to be a 32 bit string");
+
+            return Encoding.ASCII.GetBytes(key);
+        }
+    }
+}
---
+++ Unite.Identity/Services/SessionService.cs
@@ -0,0 +1,48 @@
+﻿using Unite.Identity.Data.Entities;
+using Unite.Identity.Data.Services;
+
+namespace Unite.Identity.Services;
+
+public class SessionService
+{
+    private readonly IdentityDbContext _dbContext;
+
+    public SessionService(IdentityDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public UserSession CreateSession(User user, string client)
+    {
+        var session = Guid.NewGuid().ToString();
+
+        var userSession = new UserSession()
+        {
+            UserId = user.Id,
+            Client = client,
+            Session = session
+        };
+
+        _dbContext.Add(userSession);
+        _dbContext.SaveChanges();
+
+        return userSession;
+    }
+
+    public UserSession FindSession(User user, string session)
+    {
+        var userSession = _dbContext.Set<UserSession>()
+            .FirstOrDefault(userSession =>
+                userSession.UserId == user.Id &&
+                userSession.Session == session
+            );
+
+        return userSession;
+    }
+
+    public void RemoveSession(UserSession session)
+    {
+        _dbContext.Remove(session);
+        _dbContext.SaveChanges();
+    }
+}
