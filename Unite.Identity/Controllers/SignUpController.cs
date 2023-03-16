8a8083b639b384c17b2250313581beb7ef7ed528
DGitRich <daniel.rich@gmx.net>
2023-03-16 17:12:39 +0100

    Create SignUpController with supplements

---
+++ Unite.Identity/Controllers/SignUpController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unite.Identity.Services;
+using Unite.Identity.Models;
+
+namespace Unite.Identity.Controllers;
+
+[Route("api/identity/[controller]")]
+public class SignUpController : Controller
+{
+    private readonly IdentityService _identityService;
+
+    public SignUpController(IdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] SignUpModel signUpModel)
+    {
+        var user = _identityService.SignUpUser(signUpModel.Email, signUpModel.Password);
+
+        return user != null ? Ok() : BadRequest($"Email address '{signUpModel.Email}' is not in access list or already registered");
+    }
+}
---
+++ Unite.Identity/Models/SignUpModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Unite.Identity.Models;
+
+public class SignUpModel
+{
+    private string _email;
+
+    public string Email
+    {
+        get { return _email?.Trim().ToLower(); }
+        set { _email = value; }
+    }
+    public string Password { get; set; }
+    public string PasswordRepeat { get; set; }
+}
