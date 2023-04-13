2061b16b4d6665ce9906634f3820151bf39546b7
DGitRich <daniel.rich@gmx.net>
2023-04-13 17:52:41 +0200

    Create RootOptions

---
+++ Unite.Identity/Configuration/Options/RootOptions.cs
@@ -0,0 +1,30 @@
+﻿namespace Unite.Identity.Configuration.Options;
+
+public class RootOptions
+{
+    public string User
+    {
+        get
+        {
+            var email = Environment.GetEnvironmentVariable("UNITE_ROOT_USER");
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentNullException("'UNITE_ROOT_USER' environment variable has to be set");
+
+            return email.Trim().ToLower();
+        }
+    }
+
+    public string Password
+    {
+        get
+        {
+            var password = Environment.GetEnvironmentVariable("UNITE_ROOT_PASSWORD");
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentNullException("'UNITE_ROOT_PASSWORD' environment variable has to be set");
+
+            return password;
+        }
+    }
+}
