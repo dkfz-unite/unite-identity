c17a4a3df3eaa75db2bacb1e76ee89e357320f53
DGitRich <daniel.rich@gmx.net>
2023-04-17 16:15:55 +0200

    Create DefaultController

---
+++ Unite.Identity.Web/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unite.Identity.Web.Controllers;
+
+[Route("api/")]
+public class DefaultController : Controller
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var date = DateTime.UtcNow;
+
+        return Json(date);
+    }
+}
