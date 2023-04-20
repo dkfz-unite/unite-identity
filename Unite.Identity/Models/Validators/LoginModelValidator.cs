b0e6c4028645414498cf2eb2efe0569426bd27c6
DGitRich <daniel.rich@gmx.net>
2023-04-20 11:24:57 +0200

    Create LoginModelValidator

---
+++ Unite.Identity/Models/Validators/LoginModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Unite.Identity.Models.Validators;
+
+public class LoginModelValidator : AbstractValidator<LoginModel>
+{
+    public LoginModelValidator()
+    {
+        RuleFor(user => user.Email)
+            .NotEmpty().WithMessage("Should not be empty");
+
+        RuleFor(user => user.Password)
+            .NotEmpty().WithMessage("Should not be empty");
+    }
+}
