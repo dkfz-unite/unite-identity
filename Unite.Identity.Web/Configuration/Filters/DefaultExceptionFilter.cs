52e4c30c94ef00b6e64479b6be88a1db6742aa3e
DGitRich <daniel.rich@gmx.net>
2023-04-20 11:15:09 +0200

    Create DefaultExceptionFilter

---
+++ Unite.Identity.Web/Configuration/Filters/DefaultExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Unite.Identity.Web.Configuration.Filters;
+
+public class DefaultExceptionFilter : IExceptionFilter
+{
+    private readonly IWebHostEnvironment _environment;
+    private readonly ILogger _logger;
+
+    public DefaultExceptionFilter(
+        IWebHostEnvironment environment,
+        ILogger<DefaultExceptionFilter> logger)
+    {
+        _environment = environment;
+        _logger = logger;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        _logger.LogError(context.Exception, context.Exception.Message);
+
+        context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+    }
+}
