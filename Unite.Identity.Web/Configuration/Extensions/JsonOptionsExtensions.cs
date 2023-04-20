036408b2ebdce2203600fcff8eff1ad2d7aeca38
DGitRich <daniel.rich@gmx.net>
2023-04-20 11:07:31 +0200

    Create JsonOptionsExtensions

---
+++ Unite.Identity.Web/Configuration/Extensions/JsonOptionsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unite.Identity.Web.Configuration.Extensions;
+
+public static class JsonOptionsExtensions
+{
+    public static void AddJsonOptions(this JsonOptions options)
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumMemberConverter());
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    }
+}
