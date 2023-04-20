8fca2559c4aa74671ac20c253a3b2531c28a153c
DGitRich <daniel.rich@gmx.net>
2023-04-20 11:14:13 +0200

    Create DefaultActionFilter

---
+++ Unite.Identity.Web/Configuration/Filters/DefaultActionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Unite.Identity.Web.Configuration.Filters;
+
+public class DefaultActionFilter : IActionFilter
+{
+    private readonly ILogger _logger;
+
+    public DefaultActionFilter(ILogger<DefaultActionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new BadRequestObjectResult(context.ModelState);
+        }
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+
+    }
+}
