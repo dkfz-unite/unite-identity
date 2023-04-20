156a425d62a3b076bffced5e555fb018fa5b5acd
DGitRich <daniel.rich@gmx.net>
2023-04-20 11:15:42 +0200

    Create MvcOptionsExtensions

---
+++ Unite.Identity.Web/Configuration/Extensions/MvcOptionsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unite.Identity.Web.Configuration.Filters;
+
+namespace Unite.Identity.Web.Configuration.Extensions;
+
+public static class MvcOptionsExtensions
+{
+    public static void AddMvcOptions(this MvcOptions options)
+    {
+        options.Filters.Add(typeof(DefaultActionFilter));
+        options.Filters.Add(typeof(DefaultExceptionFilter));
+    }
+}
