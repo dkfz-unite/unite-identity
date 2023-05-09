e6d535c2de3139fa068acd61b0570fd2a02637fe
DGitRich <daniel.rich@gmx.net>
2023-05-09 17:56:15 +0200

    Update IdentityDefaultController

--- Unite.Identity.Web/Controllers/Identity/IdentityDefaultController.cs
+++ Unite.Identity.Web/Controllers/Identity/IdentityDefaultController.cs
@@ -5,43 +5,44 @@ using Unite.Identity.Web.Configuration.Options;
 using Unite.Identity.Models;
 using Unite.Identity.Services;
 using Unite.Identity.Web.Helpers;
+using Unite.Identity.Data.Entities;
 
 namespace Unite.Identity.Web.Controllers.Identity;
 
-[Route("api/default/[action]")]
+[Route("api/default")]
 public class IdentityDefaultController : ControllerBase
 {
     private readonly ApiOptions _apiOptions;
-    private readonly DefaultIdentityService _identityService;
+    private readonly DefaultIdentityService _defaultIdentityService;
     private readonly SessionService _sessionService;
     private readonly ILogger _logger;
 
     public IdentityDefaultController(
         ApiOptions apiOptions,
-        DefaultIdentityService identityService,
+        DefaultIdentityService defaultIdentityService,
         SessionService sessionService,
         ILogger<IdentityDefaultController> logger)
     {
         _apiOptions = apiOptions;
-        _identityService = identityService;
+        _defaultIdentityService = defaultIdentityService;
         _sessionService = sessionService;
         _logger = logger;
     }
 
     //(string userId, string userPass)
 
-    [HttpPost]
+    [HttpPost("register")]
     public IActionResult Register([FromBody] RegisterModel registerModel)
     {
-        var user = _identityService.RegisterUser(registerModel.Email, registerModel.Password);
+        var user = _defaultIdentityService.RegisterUser(registerModel.Email, registerModel.Password);
 
         return user != null ? Ok() : BadRequest($"Email address '{registerModel.Email}' is not in access list or already registered");
     }
 
-    [HttpPost]
+    [HttpPost("login")]
     public IActionResult Login([FromBody] LoginModel loginModel, [FromHeader(Name = "User-Agent")] string client)
     {
-        var user = _identityService.LoginUser(loginModel.Email, loginModel.Password);
+        var user = _defaultIdentityService.LoginUser(loginModel.Email, loginModel.Password);
 
         if (user == null)
         {
@@ -64,7 +65,7 @@ public class IdentityDefaultController : ControllerBase
     }
 
     [Authorize]
-    [HttpPost]
+    [HttpPost("logout")]
     public IActionResult Logout()
     {
         var session = CookieHelper.GetSessionCookie(Request);
@@ -73,7 +74,7 @@ public class IdentityDefaultController : ControllerBase
         {
             var email = ClaimsHelper.GetValue(User.Claims, ClaimTypes.Email);
 
-            var user = _identityService.GetUser(email);
+            var user = _defaultIdentityService.GetUser(email);
 
             if (user == null)
             {
@@ -94,6 +95,24 @@ public class IdentityDefaultController : ControllerBase
             _sessionService.RemoveSession(userSession);
 
             CookieHelper.DeleteSessionCookie(Response);
+        }
+
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpPut("change-password")]
+    public IActionResult Put([FromBody] PasswordChangeModel model)
+    {
+        var email = HttpContext.User.Claims.First(claim => claim.Type == ClaimTypes.Email).Value;
+
+        var currentUser = _defaultIdentityService.GetUser(email);
+
+        var updatedUser = _defaultIdentityService.ChangePassword(currentUser.Email, model.OldPassword, model.NewPassword);
+
+        if (updatedUser == null)
+        {
+            return BadRequest($"Invalid old password");
         }
 
         return Ok();
