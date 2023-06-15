ba521946d0d4b9867885cdd2734de528c7accb8b
DGitRich <daniel.rich@gmx.net>
2023-06-15 13:50:13 +0200

    Create ProviderModelValidator

---
+++ Unite.Identity/Models/Validators/ProviderModelValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentValidation;
+
+namespace Unite.Identity.Models.Validators;
+
+public class AddProviderModelValidator : AbstractValidator<AddProviderModel>
+{
+    public AddProviderModelValidator()
+    {
+        RuleFor(model => model.Name)
+            .NotEmpty().WithMessage("Should not be empty")
+            .MaximumLength(100).WithMessage("Maximum length is 100");
+
+        RuleFor(model => model.Label)
+            .MaximumLength(100).WithMessage("Maximum length is 100");
+
+        RuleFor(model => model.IsActive)
+            .NotEmpty().WithMessage("Should not be empty");
+    }
+}
+
+public class EditProviderModelValidator : AbstractValidator<EditProviderModel>
+{
+    public EditProviderModelValidator()
+    {
+        RuleFor(model => model.Id)
+            .NotEmpty().WithMessage("Should not be empty");
+
+        RuleFor(model => model.Name)
+            .NotEmpty().WithMessage("Should not be empty")
+            .MaximumLength(100).WithMessage("Maximum length is 100");
+
+        RuleFor(model => model.Label)
+            .MaximumLength(100).WithMessage("Maximum length is 100");
+
+        RuleFor(model => model.IsActive)
+            .NotEmpty().WithMessage("Should not be empty");
+    }
+}
