8a3bfdc505be19b2a29caad5e1e97503c0027ca3
DGitRich <daniel.rich@gmx.net>
2023-06-15 13:49:42 +0200

    Create ProviderModel

---
+++ Unite.Identity/Models/ProviderModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Unite.Identity.Models;
+
+public class AddProviderModel
+{
+    public string Name { get; set; }
+    public string Label { get; set; }
+    public bool IsActive { get; set; }
+    public int? Priority { get; set; }
+}
+
+public class EditProviderModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Label { get; set; }
+    public bool IsActive { get; set; }
+    public int? Priority { get; set; }
+}
