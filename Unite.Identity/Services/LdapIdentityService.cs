592bf140d5e8f4ea24685519b1e0ec77da69e80d
DGitRich <daniel.rich@gmx.net>
2023-07-14 15:30:48 +0200

    Update LdapIdentityService

--- Unite.Identity/Services/LdapIdentityService.cs
+++ Unite.Identity/Services/LdapIdentityService.cs
@@ -8,68 +8,32 @@ namespace Unite.Identity.Services;
 
 public class LdapIdentityService : BaseIdentityService, IIdentityService
 {
-    private readonly ILdapOptions _options;
-
-    private LDAPAuthentication _ldapAuth;
-    private LDAPAuthentication ldapAuth
-    {
-        get
-        {
-            if (_ldapAuth == null)
-            {
-                int? finalPortNumber = null;
-                if (_options.Port != null)
-                {
-                    finalPortNumber = Int16.Parse(_options.Port);
-                }
-
-                _ldapAuth = new LDAPAuthentication(_options.Server, _options.ServiceUserRNA, _options.ServiceUserPassword, _options.UserTargetOU, finalPortNumber);
-            }
-
-            return _ldapAuth;
-        }
-    }
+    private readonly LDAPAuthentication _ldapAuth;
 
     public LdapIdentityService(
         IdentityDbContext dbContext,
         ILdapOptions options) : base(dbContext)
     {
-        _options = options;
+        _ldapAuth = new LDAPAuthentication(options.Server, options.ServiceUserRNA, options.ServiceUserPassword, options.UserTargetOU, options.Port);
     }
 
     public User LoginUser(string userIdentifier, string userPass)
     {
-        bool userIdIsEmail = userIdentifier.Contains("@");
-        string userMail = userIdentifier;
-        if (!userIdIsEmail)
+        string userEmail = userIdentifier.Contains("@")
+            ? userIdentifier
+            : GetUserEmail(userIdentifier);
+        if (userEmail == null)
         {
-            var result = ldapAuth.ReadUserEntry(userIdentifier);
-            if (result != null)
-            {
-                try
-                {
-                    userMail = result.Attributes["mail"][0].ToString();
-                }
-                catch (Exception ex)
-                {
-                    return null;
-                }
-            }
+            return null;
         }
 
-        var user = _dbContext.Set<User>()
-            .Include(user => user.UserPermissions)
-            .Include(user => user.Provider)
-            .FirstOrDefault(user =>
-                user.Email == userMail
-                && user.Provider.Name == "LDAP");
-
+        var user = GetUser(userEmail, Providers.Ldap);
         if (user == null)
         {
             return null;
         }
 
-        var userCredentialsValid = ldapAuth.UserCredentialsValid(userIdentifier, userPass);
+        var userCredentialsValid = _ldapAuth.UserCredentialsValid(userIdentifier, userPass);
         if (!userCredentialsValid)
         {
             return null;
@@ -78,24 +42,19 @@ public class LdapIdentityService : BaseIdentityService, IIdentityService
         return user;
     }
 
-    public bool UserAuthentication(string userIdentifier, string userPass)
+    private string GetUserEmail(string userIdentifier)
     {
-        var decodedBase64Pass = Encoding.UTF8.GetString(Convert.FromBase64String(userPass));
-        var userValid = false;
-        try
+        string userEmail = null;
+        var result = _ldapAuth.ReadUserEntry(userIdentifier);
+        if (result != null)
         {
-            userValid = ldapAuth.UserCredentialsValid(userIdentifier, decodedBase64Pass);
-        }
-        catch (Exception ex)
-        {
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            try
             {
-                Console.WriteLine($"UserLdapAuthentication-UserCredentialsValid: {ex}");
+                userEmail = result.Attributes["mail"][0].ToString();
             }
+            catch (Exception ex) { }
         }
 
-        return userValid;
+        return userEmail;
     }
-
-
 }
