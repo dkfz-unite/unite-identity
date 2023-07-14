6062735e0a4095ee129cc233b61f77ad1cc7d471
DGitRich <daniel.rich@gmx.net>
2023-07-14 16:13:30 +0200

    Create ProviderResource

---
+++ Unite.Identity/Resources/ProviderResource.cs
@@ -0,0 +1,27 @@
+﻿using Unite.Identity.Data.Entities;
+
+namespace Unite.Identity.Resources;
+
+public class ProviderResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Label { get; set; }
+    public int? Priority { get; set; }
+    public string Title
+    {
+        get
+        {
+            return Label ?? Name;
+        }
+    }
+
+    public ProviderResource(Provider provider)
+    {
+        Id = provider.Id;
+        Name = provider.Name;
+        Label = provider.Label;
+        Priority = provider.Priority;
+    }
+}
+
